13483ad174f77f40d9446f10f354d9656082ca08
Maksym <maksskazka1997@gmail.com>
2026-04-21 23:54:16 +0200

    create

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,14 @@
+import Link from 'next/link';
 import css from './NotFound.module.css'; 
-
 
 export default function NotFound() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1 className={css.title}>404 - Page not found</h1>
-      <p className={css.description}>Sorry, the page you are looking for does not exist.</p>
+    <div className={css.container}>
+      <h1 className={css.title}>404</h1>
+      <p className={css.message}>Oops! The page you are looking for doesn`t exist.</p>
+      <Link href="/notes/filter/all" className={css.link}>
+        Back to Notes
+      </Link>
     </div>
   );
 }
