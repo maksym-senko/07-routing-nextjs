053f98229dbeb99f18f56f62225be699c50faee3
Maksym <maksskazka1997@gmail.com>
2026-04-25 01:15:26 +0200

    create

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,10 +1,9 @@
 import Link from 'next/link';
 import css from './Sidebar.module.css';
 
-
 const TAGS = [
   { name: 'All notes', value: 'all' },
-  { name: 'Todo', value: 'Todo' }, 
+  { name: 'Todo', value: 'Todo' },
   { name: 'Work', value: 'Work' },
   { name: 'Personal', value: 'Personal' },
   { name: 'Meeting', value: 'Meeting' },
@@ -20,7 +19,7 @@ export default function SidebarDefault() {
           {TAGS.map((tag) => (
             <li key={tag.value}>
               <Link
-                href={tag.value === 'all' ? '/notes' : `/notes/filter/${tag.value}`}
+                href={`/notes/filter/${tag.value}`}
                 className={css.link}
               >
                 {tag.name}
